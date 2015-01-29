9633b7f88eed277ce7fdb87ec2933bf86f6983f3
funkybob <curtis@tinbrain.net>
2015-01-29 21:20:47 +1100

    Added thread_list view

--- dequorum/views.py
+++ dequorum/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Thread
+
+
+def thread_list(request):
+
+    threads = Thread.objects.visible()
+
+    return render(request, 'dequorum/thread_list.html', {
+        'threads': threads,
+    })
