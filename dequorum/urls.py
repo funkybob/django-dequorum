ded62b9ae4790185f3863744f6f440ffaa6d5a3f
Olivier Harris <o.harris@kabucreative.com.au>
2015-03-08 17:00:49 +1100

    Move message creation form into thread detail view

--- dequorum/urls.py
+++ dequorum/urls.py
@@ -5,6 +5,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.thread_list, name='thread-list'),
     url(r'^add/$', views.thread_create, name='thread-create'),
-    url(r'^(?P<thread_pk>\d+)/$', views.thread_detail, name='thread-detail'),
-    url(r'^(?P<thread_pk>\d+)/add/$', views.message_create, name='message-create'),
+    url(r'^(?P<thread_pk>\d+)/$', views.thread_detail, name='thread-detail')
 ]
