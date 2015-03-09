ee06e0f44d7c009ceecdfc2aa444b421bb6be0da
Curtis Maloney <curtis@tinbrain.net>
2015-03-09 04:35:33 +0000

    Add custom admin class for Thread

--- dequorum/admin.py
+++ dequorum/admin.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 
 from . import models
 
+class ThreadAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created', 'owner', 'is_open', 'is_visible',)
+    list_filter = ('created', 'owner', 'is_open', 'is_visible',)
+
 admin.site.register(models.Tag)
 admin.site.register(models.Message)
-admin.site.register(models.Thread)
+admin.site.register(models.Thread, ThreadAdmin)
